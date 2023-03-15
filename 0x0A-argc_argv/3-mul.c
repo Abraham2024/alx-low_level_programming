aa825724d2fa674cc00fdc312213470eccdff5ba
Muhammed Abraham Amanokhai <drabraham2024@gmail.com>
2023-03-15 12:33:55 -0700

    argc

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -8,7 +8,6 @@
  * @argv: Pointer of array of pointers containing strings entering main
  * Return: Always 0 (Success)
  */
-// program to multiply two numbers
 int main(int argc, char **argv)
 {
 	int n, ex;
