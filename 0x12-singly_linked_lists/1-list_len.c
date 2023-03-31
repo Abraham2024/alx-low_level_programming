c6f0a70f16fab8086e3788c65489ad73ab9d6c36
drabraham2024 <ibrahimjunior08@gmail.com>
2023-03-30 18:00:10 -0700

    betty update

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -7,12 +7,15 @@
  * @h: the first node
  * Return: elements number
  */
-size_t list_len(const list_t *h) 
+size_t list_len(const list_t *h)
+
 {
-    size_t len = 0;
-    while (h != NULL) {
-        len++;
-        h = h->next;
-    }
-    return len;
+	size_t len = 0;
+
+	while (h != NULL)
+	{
+		len++;
+		h = h->next;
+	}
+	return (len);
 }
