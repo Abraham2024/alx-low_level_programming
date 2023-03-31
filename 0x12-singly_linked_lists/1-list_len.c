947325f6e0accd6d0a4f870cef21c7aef9be9911
drabraham2024 <ibrahimjunior08@gmail.com>
2023-03-30 17:52:42 -0700

    update

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * list_len - list length
+ * @h: the first node
+ * Return: elements number
+ */
+size_t list_len(const list_t *h) 
+{
+    size_t len = 0;
+    while (h != NULL) {
+        len++;
+        h = h->next;
+    }
+    return len;
+}
