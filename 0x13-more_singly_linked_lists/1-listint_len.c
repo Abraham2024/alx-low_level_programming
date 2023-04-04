b683ddb1a1c7e0b6b88687cd2bea17668cc8230c
drabraham2024 <ibrahimjunior08@gmail.com>
2023-04-04 10:22:15 -0700

    update

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -8,12 +8,12 @@
 
 size_t listint_len(const listint_t *h)
 {
-	size_t count = 0;
+	size_t nodes = 0;
 
 	while (h != NULL)
 	{
-		count ++;
+		nodes ++;
 		h = h->next;
 	}
-	return (count);
+	return (nodes);
 }
